d4c310f4dd584ff6d3bfcf99ec47b1a8ae471a3b
adriannicolai <adriannicolai_21@yahoo.com>
2023-11-24 12:51:13 +0800

    add --use-fake-ui-for-media-stream and --use-fake-device-for-media-stream to chromeOptions

--- __tests__/chromedriver.ts
+++ __tests__/chromedriver.ts
@@ -1,9 +1,17 @@
 import { Builder, Capabilities, until, By, Key } from 'selenium-webdriver';
+import { Options } from 'selenium-webdriver/chrome';
+
 (async () => {
   const timeout = 30000;
+  const chromeOptions = new Options();
+  chromeOptions.addArguments('--use-fake-ui-for-media-stream');
+  chromeOptions.addArguments('--use-fake-device-for-media-stream');
+
   const driver = new Builder()
     .withCapabilities(Capabilities.chrome())
+    .setChromeOptions(chromeOptions)
     .build();
+
   try {
     await driver.get('https://google.com');
     await driver.wait(until.titleContains('Google'), timeout);
